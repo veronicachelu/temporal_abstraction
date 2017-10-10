19cb140da11a7381549d0976366a5fdc1649cb6b
ioanachelu <ioana.chelu28@gmail.com>
2017-10-10 19:06:49 +0300

    Added mdps and env wrapper for gridworld in 2D

---
+++ env_wrappers/txt_wrapper.py
@@ -0,0 +1,230 @@
+import scipy.ndimage
+import tkinter
+from PIL import Image
+from PIL import ImageTk
+import numpy as np
+from PIL import Image
+import scipy.ndimage
+import numpy as np
+from gym import spaces
+
+class GridWorld:
+  def __init__(self, load_path=None):
+    self.action_space = spaces.Discrete(4)
+
+    self.rewardFunction = None
+    self.nb_actions = 4
+    if load_path != None:
+      self.read_file(load_path)
+
+    self.observation_space = spaces.Box(low=0,
+                                        high=255,
+                                        shape=(self.nb_rows, self.nb_cols, 3))
+    self.agentX, self.agentY = self.startX, self.startY
+    self.nb_states = self.nb_rows * self.nb_cols
+
+    self.win = tkinter.Toplevel()
+
+    screen_width = self.win.winfo_screenwidth()
+    screen_height = self.win.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = screen_width + 100
+    y = screen_height + 100
+
+    self.win.geometry('%sx%s+%s+%s' % (512, 512, x, y))
+    self.win.title("Gridworld")
+
+  def render(self):
+    time.sleep(0.1)
+    screen = scipy.misc.imresize(self.pix_state, [200, 200, 3], interp='nearest')
+    screen = Image.fromarray(screen, 'RGB')
+    screen = screen.resize((512, 512))
+    screen_width = self.win.winfo_screenwidth()
+    screen_height = self.win.winfo_screenheight()
+    # x = screen_width + 100
+    # y = screen_height + 100
+    #
+    # self.win.geometry('%sx%s+%s+%s' % (512, 512, x, y))
+
+    tkpi = ImageTk.PhotoImage(screen)
+    label_img = tkinter.Label(self.win, image=tkpi)
+    label_img.place(x=0, y=0,
+                    width=screen.size[0], height=screen.size[1])
+
+    # self.win.mainloop()            # wait until user clicks the window
+    self.win.update_idletasks()
+    self.win.update()
+
+  def build_screen(self):
+    mdp_screen = np.array(self.MDP)
+    mdp_screen = np.expand_dims(mdp_screen, 2)
+    mdp_screen[mdp_screen == -1] = 255
+    mdp_screen = np.tile(mdp_screen, [1, 1, 3])
+    mdp_screen[self.agentX, self.agentY] = [0, 0, 255]
+    mdp_screen[self.goalX, self.goalY] = [255, 0, 0]
+    self.pix_state = mdp_screen
+    return self.pix_state
+
+  def reset(self):
+    s = self.get_initial_state()
+    screen = self.build_screen()
+
+    return screen
+
+  def read_file(self, load_path):
+    with open(load_path, "r") as f:
+      lines = f.readlines()
+    self.nb_rows, self.nb_cols = lines[0].split(',')
+    self.nb_rows, self.nb_cols = int(self.nb_rows), int(self.nb_cols)
+    self.MDP = np.zeros((self.nb_rows, self.nb_cols))
+    lines = lines[1:]
+    for i in range(self.nb_rows):
+      for j in range(self.nb_cols):
+        if lines[i][j] == '.':
+          self.MDP[i][j] = 0
+        elif lines[i][j] == 'X':
+          self.MDP[i][j] = -1
+        elif lines[i][j] == 'S':
+          self.MDP[i][j] = 0
+          self.startX = i
+          self.startY = j
+        else:  # 'G'
+          self.MDP[i][j] = 0
+          self.goalX = i
+          self.goalY = j
+
+  def get_state_index(self, x, y):
+    idx = y + x * self.nb_cols
+    return idx
+
+  def get_initial_state(self):
+    agent_state_index = self.get_state_index(self.startX, self.startY)
+    self.agentX, self.agentY = self.startX, self.startY
+    return agent_state_index
+
+  def get_next_state(self, a):
+    action = ["up", "right", "down", "left", 'terminate']
+    nextX, nextY = self.agentX, self.agentY
+
+    if action[a] == 'terminate':
+      return -1, -1
+
+    if self.MDP[self.agentX][self.agentY] != -1:
+      if action[a] == 'up' and self.agentX > 0:
+        nextX, nextY = self.agentX - 1, self.agentY
+      elif action[a] == 'right' and self.agentY < self.nb_cols - 1:
+        nextX, nextY = self.agentX, self.agentY + 1
+      elif action[a] == 'down' and self.agentX < self.nb_rows - 1:
+        nextX, nextY = self.agentX + 1, self.agentY
+      elif action[a] == 'left' and self.agentY > 0:
+        nextX, nextY = self.agentX, self.agentY - 1
+
+    if self.MDP[nextX][nextY] != -1:
+      return nextX, nextY
+    else:
+      return self.agentX, self.agentY
+
+  def is_terminal(self, nextX, nextY):
+    if nextX == self.goalX and nextY == self.goalY:
+      return True
+    else:
+      return False
+
+  def get_next_reward(self, nextX, nextY):
+    if self.rewardFunction is None:
+      if nextX == self.goalX and nextY == self.goalY:
+        reward = 1
+      else:
+        reward = 0
+    else:
+      currStateIdx = self.get_state_index(self.agentX, self.agentY)
+      nextStateIdx = self.get_state_index(nextX, nextY)
+
+      reward = self.rewardFunction[nextStateIdx] \
+               - self.rewardFunction[currStateIdx]
+
+    return reward
+
+  def get_state_xy(self, idx):
+    y = idx % self.nb_cols
+    x = int((idx - y) / self.nb_rows)
+
+    return x, y
+
+  def get_next_state_and_reward(self, currState, a):
+    if currState == self.nb_states:
+      return currState, 0
+
+    tmpx, tmpy = self.agentX, self.agentY
+    self.agentX, self.agentY = self.get_state_xy(currState)
+    nextX, nextY = self.agentX, self.agentY
+
+    nextStateIdx = None
+    reward = None
+
+    nextX, nextY = self.get_next_state(a)
+    if nextX != -1 and nextY != -1:  # If it is not the absorbing state:
+      reward = self.get_next_reward(nextX, nextY)
+      nextStateIdx = self.get_state_index(nextX, nextY)
+    else:
+      reward = 0
+      nextStateIdx = self.nb_states
+
+    self.agentX, self.agentY = tmpx, tmpy
+
+    return nextStateIdx, reward
+
+  def step(self, a):
+    nextX, nextY = self.get_next_state(a)
+
+    self.agentX, self.agentY = nextX, nextY
+
+    done = False
+    if self.is_terminal(nextX, nextY):
+      done = True
+
+    reward = self.get_next_reward(nextX, nextY)
+    nextStateIdx = self.get_state_index(nextX, nextY)
+
+    screen = self.build_screen()
+
+    return screen, reward, done, None
+
+  def get_action_set(self):
+    return range(0, 4)
+
+  def define_reward_function(self, vector):
+    self.rewardFunction = vector
+
+
+if __name__ == '__main__':
+
+  import time
+  import tkinter
+  from PIL import Image
+  from PIL import ImageTk
+  import numpy as np
+
+  player_rng = np.random.RandomState(0)
+  game = GridWorld("/home/ioana/turi/rl/AOC/mdps/4rooms.mdp")
+
+  start = time.time()
+  # reward_color = [np.random.uniform(), np.random.uniform(), np.random.uniform()]
+  # reward_color = [1,0,0]
+  s = game.reset()
+  ep = 0
+  step = 0
+  tot_rw = 0
+
+  while True:
+    s1, r, d, _ = game.step(player_rng.choice(4))
+    step += 1
+    game.render()
+    tot_rw += r
+    if d:
+      ep += 1
+      s = game.reset()
+
+  print("Finished %d episodes in %d steps in %.2f. Total reward: %d.",
+        (ep, step, time.time() - start, tot_rw))
--- networks.py
+++ networks.py
@@ -178,6 +178,175 @@ class AOCNetwork(tf.contrib.rnn.RNNCell):
       o_terminations = o_terminations > local_random
     return o_terminations
 
+class SFNetwork(tf.contrib.rnn.RNNCell):
+
+  def __init__(self, scope, config, action_size):
+    self._scope = scope
+    self._conv_layers = config.conv_layers
+    self._fc_layers = config.fc_layers
+    self._action_size = action_size
+    self._nb_options = config.nb_options
+    self._nb_envs = config.num_agents
+    self._config = config
+    self._network_optimizer = config.network_optimizer(
+        self._config.lr, name='network_optimizer')
+    self._exploration_options = TFLinearSchedule(self._config.explore_steps, self._config.final_random_action_prob,
+                                                 self._config.initial_random_action_prob)
+
+    with tf.variable_scope(scope):
+      self.observation = tf.placeholder(shape=[None, 84, 84, 4],
+                                   dtype=tf.float32, name="Inputs")
+      self.total_steps = tf.placeholder(shape=[], dtype=tf.int32, name="total_steps")
+
+      self.image_summaries = tf.summary.image('input', self.observation, max_outputs=1)
+      self.summaries = []
+      with tf.variable_scope('conv'):
+        for i, (kernel_size, stride, nb_kernels) in enumerate(self._conv_layers):
+          out = layers.conv2d(self.observation, num_outputs=nb_kernels, kernel_size=kernel_size,
+                              stride=stride, activation_fn=tf.nn.relu,
+                              variables_collections=tf.get_collection("variables"),
+                              outputs_collections="activations", scope="conv_{}".format(i))
+          self.summaries.append(tf.contrib.layers.summarize_activation(out))
+
+        out = layers.flatten(out, scope="flatten")
+        with tf.variable_scope("fc"):
+          for i, nb_filt in enumerate(self._fc_layers):
+            out = layers.fully_connected(out, num_outputs=nb_filt,
+                                             activation_fn=None,
+                                             variables_collections=tf.get_collection("variables"),
+                                             outputs_collections="activations", scope="fc_{}".format(i))
+            out = layer_norm_fn(out, relu=True)
+            self.summaries.append(tf.contrib.layers.summarize_activation(out))
+
+        with tf.variable_scope("option_term"):
+          self.termination = layers.fully_connected(out, num_outputs=self._nb_options,
+                                                                    activation_fn=tf.nn.sigmoid,
+                                                                    variables_collections=tf.get_collection("variables"),
+                                                                    outputs_collections="activations")
+          self.summaries.append(tf.contrib.layers.summarize_activation(self.termination))
+
+        with tf.variable_scope("q_val"):
+          self.q_val = layers.fully_connected(out, num_outputs=self._nb_options,
+                                                      activation_fn=None,
+                                                      variables_collections=tf.get_collection("variables"),
+                                                      outputs_collections="activations")
+          self.summaries.append(tf.contrib.layers.summarize_activation(self.q_val))
+
+          max_options = tf.cast(tf.argmax(self.q_val, 1), dtype=tf.int32)
+          exp_options = tf.random_uniform(shape=[1], minval=0, maxval=self._config.nb_options,
+                                          dtype=tf.int32)
+          local_random = tf.random_uniform(shape=[1], minval=0., maxval=1., dtype=tf.float32,
+                                           name="rand_options")
+          probability_of_random_option = self._exploration_options.value(self.total_steps)
+          condition = local_random > tf.tile(probability_of_random_option[None, ...], [1])
+          self.current_option = tf.where(condition, max_options, exp_options)
+          self.v = tf.reduce_max(self.q_val, axis=1) * (1 - probability_of_random_option) + \
+              probability_of_random_option * tf.reduce_mean(self.q_val, axis=1)
+
+        with tf.variable_scope("i_o_policies"):
+          self.options = []
+          for _ in range(self._nb_options):
+            option = layers.fully_connected(out, num_outputs=self._action_size,
+                                                activation_fn=tf.nn.softmax,
+                                                variables_collections=tf.get_collection("variables"),
+                                                outputs_collections="activations")
+
+            self.summaries.append(tf.contrib.layers.summarize_activation(option))
+            self.options.append(option)
+          self.options = tf.stack(self.options, 1)
+
+        if scope != 'global':
+          self.actions_placeholder = tf.placeholder(shape=[None], dtype=tf.int32, name="Actions")
+          self.options_placeholder = tf.placeholder(shape=[None], dtype=tf.int32, name="Options")
+          self.target_return = tf.placeholder(shape=[None], dtype=tf.float32)
+          self.target_v = tf.placeholder(shape=[None], dtype=tf.float32)
+          self.delib = tf.placeholder(shape=[], dtype=tf.float32)
+
+          policy = self.get_intra_option_policy(self.options_placeholder)
+          responsible_outputs = self.get_responsible_outputs(policy, self.actions_placeholder)
+          q_val = self.get_q(self.options_placeholder)
+          termination = self.get_o_term(self.options_placeholder)
+
+          with tf.name_scope('critic_loss'):
+            td_error = tf.stop_gradient(self.target_return) - q_val
+            self.critic_loss = tf.reduce_mean(self._config.critic_coef * 0.5 * tf.square((td_error)))
+          with tf.name_scope('termination_loss'):
+            self.term_loss = tf.reduce_mean(termination * (tf.stop_gradient(q_val) - self.target_v + self.delib))
+          with tf.name_scope('entropy_loss'):
+            self.entropy_loss = self._config.entropy_coef * tf.reduce_mean(tf.reduce_sum(policy *
+                                                                                           tf.log(policy +
+                                                                                    1e-7), axis=1))
+          with tf.name_scope('policy_loss'):
+            self.policy_loss = -tf.reduce_sum(tf.log(responsible_outputs + 1e-7) * td_error)
+
+          self.loss = self.policy_loss + self.entropy_loss + self.critic_loss + self.term_loss
+
+          local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+          gradients = tf.gradients(self.loss, local_vars)
+          self.var_norms = tf.global_norm(local_vars)
+          grads, self.grad_norms = tf.clip_by_global_norm(gradients, self._config.gradient_clip_value)
+
+          # for grad, weight in zip(grads, local_vars):
+          #   self.summaries.append(tf.summary.histogram(weight.name + '_grad', grad))
+          #   self.summaries.append(tf.summary.histogram(weight.name, weight))
+
+          self.merged_summary = tf.summary.merge([tf.summary.scalar('avg_critic_loss', tf.reduce_mean(self.critic_loss)),
+                                                  tf.summary.scalar('avg_termination_loss', tf.reduce_mean(self.term_loss)),
+                                                  tf.summary.scalar('avg_entropy_loss', tf.reduce_mean(self.entropy_loss)),
+                                                  tf.summary.scalar('avg_policy_loss', tf.reduce_mean(self.policy_loss)),
+                                                  tf.summary.scalar('gradient_norm', tf.global_norm(gradients)),
+                                                  tf.summary.scalar('cliped_gradient_norm', tf.global_norm(grads)),
+                                                  gradient_summaries(zip(grads, local_vars))] + self.summaries)
+
+          global_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'global')
+          self.apply_grads = self._network_optimizer.apply_gradients(zip(grads, global_vars))
+
+  def get_intra_option_policy(self, options):
+    current_option_option_one_hot = tf.one_hot(options, self._nb_options, dtype=tf.float32, name="options_one_hot")
+    current_option_option_one_hot = tf.tile(current_option_option_one_hot[..., None], [1, 1, self._action_size])
+    action_probabilities = tf.reduce_sum(tf.multiply(self.options, current_option_option_one_hot),
+                                         reduction_indices=1, name="P_a")
+    return action_probabilities
+
+  def get_responsible_outputs(self, policy, actions):
+    actions_onehot = tf.one_hot(actions, self._action_size, dtype=tf.float32,
+                                     name="actions_one_hot")
+    responsible_outputs = tf.reduce_sum(policy * actions_onehot, [1])
+    return responsible_outputs
+
+  def get_policy_over_options(self, batch_size, probability_of_random_option):
+    max_options = tf.cast(tf.argmax(self.q_val, 1), dtype=tf.int32)
+    exp_options = tf.random_uniform(shape=[batch_size], minval=0, maxval=self._config.nb_options,
+                                    dtype=tf.int32)
+    local_random = tf.random_uniform(shape=[batch_size], minval=0., maxval=1., dtype=tf.float32, name="rand_options")
+    condition = local_random > tf.tile(probability_of_random_option[None, ...], [batch_size])
+    options = tf.where(condition, max_options, exp_options)
+
+    return options
+
+  def get_action(self, o):
+    current_option_option_one_hot = tf.one_hot(o, self._config.nb_options, name="options_one_hot")
+    current_option_option_one_hot = current_option_option_one_hot[:, :, None]
+    current_option_option_one_hot = tf.tile(current_option_option_one_hot, [1, 1, self._action_size])
+    self.action_probabilities = tf.reduce_sum(tf.multiply(self.options, current_option_option_one_hot),
+                                              reduction_indices=1, name="P_a")
+    policy = tf.multinomial(tf.log(self.action_probabilities), 1)[:, 0]
+    return policy
+
+  def get_q(self, o):
+    current_option_option_one_hot = tf.one_hot(o, self._config.nb_options, name="options_one_hot")
+    q_values = tf.reduce_sum(tf.multiply(self.q_val, current_option_option_one_hot),
+                             reduction_indices=1, name="Values_Q")
+    return q_values
+
+  def get_o_term(self, o, boolean_value=False):
+    current_option_option_one_hot = tf.one_hot(o, self._config.nb_options, name="options_one_hot")
+    o_terminations = tf.reduce_sum(tf.multiply(self.termination, current_option_option_one_hot),
+                                   reduction_indices=1, name="O_Terminations")
+    if boolean_value:
+      local_random = tf.random_uniform(shape=[], minval=0., maxval=1., dtype=tf.float32, name="rand_o_term")
+      o_terminations = o_terminations > local_random
+    return o_terminations
 
 def layer_norm_fn(x, relu=True):
   x = layers.layer_norm(x, scale=True, center=True)
