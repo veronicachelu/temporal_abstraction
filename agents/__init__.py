767150936f49c3d0863a0263f9f4cc3a2f2fedd8
ioanachelu <ioana.chelu28@gmail.com>
2018-02-02 11:14:28 +0200

    small fix

--- agents/__init__.py
+++ agents/__init__.py
@@ -5,13 +5,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .aoc_agent import AOCAgent
-from .sf_agent import SFAgent
-from .ac_agent import ACAgent
-from .ac_sf_agent import ACSFAgent
-from .ac_option_agent import ACOptionAgent
-from .ac_matrix_agent import ACMatrixAgent
-from .tabular_sf_agent import TabSFAgent
 from .linear_sf_agent import LinearSFAgent
 from .dif_agent import DIFAgent
 from .eigenoc_agent import EigenOCAgent
