35c9f48fe3b44c3e4dc9f6bcaf8cb73171db5a01
Ioana Chelu <ioana.chelu28@gmail.com>
2018-03-27 09:20:56 +0300

    small fix for eigenoc classic

--- configs.py
+++ configs.py
@@ -118,7 +118,7 @@ def dynamic_SR():
   training_steps = 5e5
   summary_interval = 10
   checkpoint_interval = 10
-  max_length = 1e6
+  max_length = 1e20
 
   return locals()
 
@@ -175,14 +175,14 @@ def eigenoc():
 def eigenoc_dyn():
   locals().update(eigenoc())
   dif_agent = EigenOCAgentDyn
-  sf_matrix_size = 10000
+  sf_matrix_size = 5000
   sr_matrix = "dynamic"
   return locals()
 
 def oc_dyn():
   locals().update(oc())
   dif_agent = EigenOCAgentDyn
-  sf_matrix_size = 10000
+  sf_matrix_size = 5000
   sr_matrix = "dynamic"
   return locals()
 
