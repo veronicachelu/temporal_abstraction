e3ee26c6da91afb20a7a7b19de167a890a7bd66b
Ioana Chelu <ioana.chelu28@gmail.com>
2018-05-06 12:37:09 +0300

    Som agent modifications [in progress]

--- agents/som_agent.py
+++ agents/som_agent.py
@@ -61,7 +61,8 @@ class SomAgent(BaseAgent):
     self.R = 0
     self.eigen_R = 0
 
-    self.sess.run(self.local_network.decrease_prob_of_random_option)
+    if self.config.decrease_option_prob:
+      self.sess.run(self.local_network.decrease_prob_of_random_option)
     # self.stats_options = np.zeros((self.nb_states, self.nb_options + self.action_size))
     # self.ms_aux = self.ms_sf = self.ms_reward = self.ms_option = None
 
