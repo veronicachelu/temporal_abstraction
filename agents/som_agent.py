ab24d171d3706145045b3a725151ae264e9d5125
Ioana Chelu <ioana.chelu28@gmail.com>
2018-05-07 16:57:08 +0300

    adding old version of eigenoc with svd

--- agents/som_agent.py
+++ agents/som_agent.py
@@ -133,7 +133,8 @@ class SomAgent(BaseAgent):
         while not coord.should_stop():
           if (self.config.steps != -1 and \
               (self.total_steps > self.config.steps and self.name == "worker_0")) or \
-            self.total_steps > len(self.config.goal_locations) * self.config.move_goal_nb_of_ep:
+              (self.episode_count > len(self.config.goal_locations) * self.config.move_goal_nb_of_ep and
+                   self.name == "worker_0"):
             coord.request_stop()
             return 0
 
