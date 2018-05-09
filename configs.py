15d626dfbb00535e9b23879c28efd02d0e62d699
Ioana Chelu <ioana.chelu28@gmail.com>
2018-05-09 20:49:43 +0300

    updates to behaviour agent/thread config

--- configs.py
+++ configs.py
@@ -160,8 +160,8 @@ def oc():
   include_primitive_options = True
   sr_matrix_size = 169
   sr_matrix = "static"
-  # goal_locations = [(11, 7), (5, 2), (1, 10), (2, 2), (6, 2)]
-  goal_locations = [(1, 11), (3, 2), (6, 2), (1, 4), (1, 1), (8, 1), (2, 5), (11, 10)]
+  goal_locations = [(11, 7), (5, 2), (1, 10), (2, 2), (6, 2)]
+  # goal_locations = [(1, 11), (3, 2), (6, 2), (1, 4), (1, 1), (8, 1), (2, 5), (11, 10)]
   move_goal_nb_of_ep = 1000
   env = functools.partial(
     GridWorld, goal_locations, "./mdps/4rooms.mdp")
