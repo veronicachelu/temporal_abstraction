dc538fefb9d6d005cbc7c224d69fe537cf7c4315
Ioana Chelu <ioana.chelu28@gmail.com>
2018-05-31 13:46:48 +0300

    choose max option for visualisation

--- configs.py
+++ configs.py
@@ -212,7 +212,7 @@ def test_options():
 
   decrease_option_prob = True
   # delib_cost_disc = 0.99
-  delib_margin = 0.012
+  delib_margin = 0.01
 
   final_random_option_prob = 0.1
   initial_random_option_prob = 1
