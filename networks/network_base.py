ba2fc3a05cf43ceea98860230eb502e2d0ca9f3b
Ioana Chelu <ioana.chelu28@gmail.com>
2018-06-06 11:04:01 +0300

    investigating option termination

--- networks/network_base.py
+++ networks/network_base.py
@@ -251,7 +251,7 @@ class BaseNetwork():
     #                                            [tf.summary.scalar('aux_loss', self.aux_loss)] + [
     #                                              tf.summary.scalar('cliped_gradient_norm_aux',
     #                                                                tf.global_norm(self.grads_aux)),
-                                                 gradient_summaries(zip(self.grads_aux, local_vars))])
+    #                                              gradient_summaries(zip(self.grads_aux, local_vars))])
     options_to_merge = self.summaries_option + [tf.summary.scalar('avg_critic_loss', self.critic_loss),
                                                 tf.summary.scalar('avg_entropy_loss', self.entropy_loss),
                                                 tf.summary.scalar('avg_policy_loss', self.policy_loss),
