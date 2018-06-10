81b83ed47155139697884d2b4866bf54d2f762b2
Ioana Chelu <ioana.chelu28@gmail.com>
2018-06-10 12:21:59 +0300

    investigating option termination

--- agents/eigenoc_agent.py
+++ agents/eigenoc_agent.py
@@ -157,6 +157,7 @@ class EigenOCAgent(BaseAgent):
           if self.config.sr_matrix is not None:
             self.load_directions()
           self.init_episode()
+          r_i = 0
 
           s = self.env.reset()
           s_idx = None
