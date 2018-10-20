e9eda6a45d6d8685a550321459829b3bbf7b9cdc
Ioana Chelu <ioana.chelu28@gmail.com>
2018-10-20 12:30:22 +0300

    Adding continuous temp abstr

---
+++ agents/attention_agent.py
@@ -0,0 +1,281 @@
+import numpy as np
+import tensorflow as tf
+from tools.agent_utils import get_mode, update_target_graph_aux, update_target_graph_sf, \
+  update_target_graph_option, discount, reward_discount, set_image, make_gif
+import os
+
+import matplotlib.patches as patches
+import matplotlib.pylab as plt
+import numpy as np
+from collections import deque
+import seaborn as sns
+
+sns.set()
+import random
+import matplotlib.pyplot as plt
+from agents.eigenoc_agent_dynamic import EigenOCAgentDyn
+import copy
+from threading import Barrier, Thread
+
+FLAGS = tf.app.flags.FLAGS
+
+"""This Agent is a specialization of the successor representation direction based agent with buffer SR matrix, but instead of choosing from discreate options that are grounded in the SR basis only by means of the pseudo-reward, it keeps a singly intra-option policy whose context is changed by means of the option given as embedding (the embedding being the direction given by the spectral decomposition of the SR matrix)"""
+class AttentionAgent(EigenOCAgentDyn):
+  def __init__(self, sess, game, thread_id, global_step, global_episode, config, global_network, barrier):
+    super(AttentionAgent, self).__init__(sess, game, thread_id, global_step, global_episode, config, global_network, barrier)
+
+  """Starting point of the agent acting in the environment"""
+  def play(self, coord, saver):
+    self.saver = saver
+
+    with self.sess.as_default(), self.sess.graph.as_default():
+      self.init_agent()
+
+      with coord.stop_on_exception():
+        while not coord.should_stop():
+          if (self.config.steps != -1 and \
+                  (self.global_step_np > self.config.steps and self.name == "worker_0")) or \
+              (self.global_episode_np > len(self.config.goal_locations) * self.config.move_goal_nb_of_ep and
+                   self.name == "worker_0" and self.config.multi_task):
+            coord.request_stop()
+            return 0
+
+          """update local network parameters from global network"""
+          self.sync_threads()
+
+          self.init_episode()
+
+          """Reset the environment and get the initial state"""
+          s = self.env.reset()
+
+          """While the episode does not terminate"""
+          while not self.done:
+            """update local network parameters from global network"""
+            self.sync_threads()
+
+            """Choose an action from the current intra-option policy"""
+            self.policy_evaluation(s)
+            # self.add_stats_to_tracker()
+
+            s1, r, self.done, self.s1_idx = self.env.step(self.action)
+
+            self.episode_reward += r
+            self.reward = np.clip(r, -1, 1)
+
+            """If the episode ended make the last state absorbing"""
+            if self.done:
+              s1 = s
+              self.s1_idx = self.s_idx
+
+            self.episode_buffer_sf.append([s, s1, self.action, self.reward, self.fi])
+            self.sf_prediction(s1)
+
+            # if not self.done:
+            #   """Increase the timesteps for the options - for statistics purposes"""
+            #   self.o_tracker_steps[self.option] += 1
+
+            """Do n-step prediction for the returns"""
+            r_mix = self.option_prediction(s, s1)
+
+            if self.total_steps % self.config.step_summary_interval == 0 and self.name == 'worker_0':
+              self.write_step_summary(r, r_mix)
+
+            s = s1
+            self.s_idx = self.s1_idx
+            self.episode_length += 1
+            self.total_steps += 1
+
+            if self.name == "worker_0":
+              self.sess.run(self.increment_global_step)
+              self.global_step_np = self.global_step.eval()
+
+          self.update_episode_stats()
+
+          if self.name == "worker_0":
+            self.sess.run(self.increment_global_episode)
+            self.global_episode_np = self.global_episode.eval()
+
+            if self.global_episode_np % self.config.checkpoint_interval == 0:
+              self.save_model()
+
+            if self.global_episode_np % self.config.summary_interval == 0:
+              self.write_summaries()
+
+          """If it's time to change the task - move the goal, wait for all other threads to finish the current task"""
+          if self.total_episodes % self.config.move_goal_nb_of_ep == 0 and \
+                  self.total_episodes != 0:
+            tf.logging.info("Moving GOAL....")
+            self.barrier.wait()
+            self.goal_position = self.env.set_goal(self.total_episodes, self.config.move_goal_nb_of_ep)
+
+          self.total_episodes += 1
+
+  """Check is the option terminates at the next state"""
+  def option_terminate(self, s1):
+    """If we took a primitive option, termination is assured"""
+    if self.config.include_primitive_options and self.primitive_action:
+      self.o_term = True
+    else:
+      feed_dict = {self.local_network.observation: [s1],
+                   self.local_network.option_direction_placeholder: [self.global_network.directions[self.option]]}
+      o_term = self.sess.run(self.local_network.termination, feed_dict=feed_dict)
+      self.prob_terms = [o_term[0]]
+      self.o_term = o_term[0] > np.random.uniform()
+
+    """Stats for tracking option termination"""
+    self.termination_counter += self.o_term * (1 - self.done)
+    self.episode_oterm.append(self.o_term)
+    self.o_tracker_len[self.option].append(self.crt_op_length)
+
+  """Sample an action from the current option's policy"""
+  def policy_evaluation(self, s):
+    feed_dict = {self.local_network.observation: [s],
+                 self.local_network.matrix_sf: [self.global_network.sf_matrix_buffer]}
+
+    tensor_list = [self.local_network.fi,
+                   self.local_network.sf,
+                   self.local_network.current_option_direction,
+                   self.local_network.eigen_q_val,
+                   self.local_network.option_policy]
+
+    results = self.sess.run(tensor_list, feed_dict=feed_dict)
+
+    fi,\
+    sf,\
+    current_option_direction,\
+    eigen_q_value,\
+    option_policy = results
+    """Add the eigen option-value function to the buffer in order to add stats to tensorboad at the end of the episode"""
+    self.eigen_q_value = eigen_q_value[0]
+    self.episode_eigen_q_values.append(self.eigen_q_value)
+    self.current_option_direction = current_option_direction[0]
+
+    """Get the intra-option policy for the current option"""
+    pi = option_policy[0]
+    """Sample an action"""
+    self.action = np.random.choice(pi, p=pi)
+    self.action = np.argmax(pi == self.action)
+
+    sf = sf[0]
+    self.fi = fi[0]
+    self.add_SF(sf)
+
+    """Store information in buffers for stats in tensorboard"""
+    self.episode_actions.append(self.action)
+
+  """Do n-step prediction for the returns and update the option policies and critics"""
+  def option_prediction(self, s, s1):
+    """construct the mixed reward signal to pass to the eigen intra-option critics."""
+    feed_dict = {self.local_network.observation: np.stack([s, s1])}
+    fi = self.sess.run(self.local_network.fi,
+                       feed_dict=feed_dict)
+    """The internal reward will be the cosine similary between the direction in latent space and the 
+         eigen direction corresponding to the current option"""
+    r_i = self.cosine_similarity((fi[1] - fi[0]), self.current_option_direction)
+    r_mix = self.config.alpha_r * r_i + (1 - self.config.alpha_r) * self.reward
+
+    """Adding to the transition buffer for doing n-step prediction on critics and policies"""
+    self.episode_buffer_option.append(
+      [s, self.current_option_direction, self.action, self.reward, r_mix, s1])
+
+    if len(self.episode_buffer_option) >= self.config.max_update_freq or self.done or (
+          self.o_term and len(self.episode_buffer_option) >= self.config.min_update_freq):
+      """Get the bootstrap option-value functions for the next time step"""
+      if self.done:
+        bootstrap_eigen_Q = 0
+      else:
+        feed_dict = {self.local_network.observation: [s1],
+                     self.local_network.matrix_sf: [self.global_network.sf_matrix_buffer]}
+        q_eigen = self.sess.run(self.local_network.eigen_q_val, feed_dict=feed_dict)
+
+        q_eigen = q_eigen[0]
+        bootstrap_eigen_Q = q_eigen
+
+      self.train_option(bootstrap_eigen_Q)
+
+      self.episode_buffer_option = []
+
+  """Do n-step prediction for the successor representation latent and an update for the representation latent using 1-step next frame prediction"""
+  def sf_prediction(self, s1):
+    if len(self.episode_buffer_sf) == self.config.max_update_freq or self.done:
+      """Get the successor features of the next state for which to bootstrap from"""
+      feed_dict = {self.local_network.observation: [s1]}
+      next_sf = self.sess.run(self.local_network.sf,
+                         feed_dict=feed_dict)[0]
+      bootstrap_sf = np.zeros_like(next_sf) if self.done else next_sf
+      self.train_sf(bootstrap_sf)
+      self.episode_buffer_sf = []
+
+  """Do one n-step update for training the agent's latent successor representation space and an update for the next frame prediction"""
+  def train_sf(self, bootstrap_sf):
+    rollout = np.array(self.episode_buffer_sf)
+    observations = rollout[:, 0]
+    next_observations = rollout[:, 1]
+    actions = rollout[:, 2]
+    rewards = rollout[:, 3]
+    fi = rollout[:, 4]
+
+    """Construct list of latent representations for the entire trajectory"""
+    sf_plus = np.asarray(fi.tolist() + [bootstrap_sf])
+    """Construct the targets for the next step successor representations for the entire trajectory"""
+    discounted_sf = discount(sf_plus, self.config.discount)[:-1]
+
+    feed_dict = {self.local_network.target_sf: np.stack(discounted_sf, axis=0),
+                 self.local_network.observation: np.stack(observations, axis=0),
+                 self.local_network.actions_placeholder: actions,
+                 self.local_network.target_next_obs: np.stack(next_observations, axis=0)}
+
+    _, self.summaries_sf, sf_loss, _, self.summaries_aux, aux_loss = \
+      self.sess.run([self.local_network.apply_grads_sf,
+                     self.local_network.merged_summary_sf,
+                     self.local_network.sf_loss,
+                     self.local_network.apply_grads_aux,
+                     self.local_network.merged_summary_aux,
+                     self.local_network.aux_loss
+                     ],
+                    feed_dict=feed_dict)
+
+  """Do n-step prediction on the critics and policies"""
+  def train_option(self, bootstrap_value_mix):
+    rollout = np.array(self.episode_buffer_option)
+    observations = rollout[:, 0]
+    option_directions = rollout[:, 1]
+    actions = rollout[:, 2]
+    rewards = rollout[:, 3]
+    eigen_rewards = rollout[:, 4]
+    next_observations = rollout[:, 5]
+
+    """Construct list of discounted returns using mixed reward signals for the entire n-step trajectory"""
+    eigen_rewards_plus = np.asarray(eigen_rewards.tolist() + [bootstrap_value_mix])
+    discounted_eigen_returns = reward_discount(eigen_rewards_plus, self.config.discount)[:-1]
+
+    feed_dict = {
+                 self.local_network.target_eigen_return: discounted_eigen_returns,
+                 self.local_network.observation: np.stack(observations, axis=0),
+                 self.local_network.actions_placeholder: actions,
+                 self.local_network.matrix_sf: [self.global_network.sf_matrix_buffer]
+                 # self.local_network.current_option_direction: option_directions,
+                 }
+
+    """Do an update on the intra-option policies"""
+    _, self.summaries_option = self.sess.run([self.local_network.apply_grads_option,
+                                       self.local_network.merged_summary_option,
+                                       ], feed_dict=feed_dict)
+
+    """Store the bootstrap target returns at the end of the trajectory"""
+    self.eigen_R = discounted_eigen_returns[-1]
+
+  def write_step_summary(self, r, r_mix=None):
+    self.summary = tf.Summary()
+    self.summary.value.add(tag='Step/Action', simple_value=self.action)
+    self.summary.value.add(tag='Step/EigenQ', simple_value=self.eigen_q_value)
+    self.summary.value.add(tag='Step/Target_EigenQ', simple_value=self.eigen_R)
+
+    self.summary_writer.add_summary(self.summary, self.total_steps)
+    self.summary_writer.flush()
+
+  def update_episode_stats(self):
+    if self.config.use_eigendirections and len(self.episode_eigen_q_values) != 0:
+      self.episode_mean_eigen_q_values.append(np.mean(self.episode_eigen_q_values))
+    if len(self.episode_actions) != 0:
+      self.episode_mean_actions.append(get_mode(self.episode_actions))
---
+++ networks/network_attention.py
@@ -0,0 +1,154 @@
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+from config_utility import gradient_summaries, huber_loss
+import numpy as np
+from networks.network_eigenoc import EignOCNetwork
+import os
+
+def normalized_columns_initializer(std=1.0):
+    def _initializer(shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+
+    return _initializer
+
+"""Function approximation network for the option critic policies and value functions when options are given as embeddings corresponding to the spectral decomposition of the SR matrix"""
+class AttentionNetwork(EignOCNetwork):
+  def __init__(self, scope, config, action_size):
+    self.goal_embedding_size = config.sf_layers[-1]
+    super(AttentionNetwork, self).__init__(scope, config, action_size)
+
+  """Build the encoder for the latent representation space"""
+  def build_feature_net(self, out):
+    self.current_option_direction_placeholder = tf.placeholder(shape=[None, self.config.sf_layers[-1]],
+                                                       dtype=tf.float32,
+                                                       name="option_direction")
+
+    super(AttentionNetwork, self).build_feature_net(out)
+    intra_features = layers.fully_connected(tf.stop_gradient(self.fi_relu),
+                                      num_outputs=self.action_size * self.goal_embedding_size,
+                                      activation_fn=None,
+                                      variables_collections=tf.get_collection("variables"),
+                                      outputs_collections="activations", scope="U")
+    self.policy_features = tf.reshape(intra_features, [-1 , self.action_size, self.goal_embedding_size],
+                               name="policy_features")
+    self.value_features = tf.identity(intra_features, name="value_features")
+
+  """Build the intra-option policies critics"""
+  def build_eigen_option_q_val_net(self):
+    with tf.variable_scope("eigen_option_q_val"):
+      value_embedding = tf.get_variable("value_embedding",
+                                     shape=[self.action_size * self.goal_embedding_size + self.goal_embedding_size, 1],
+                                     initializer=normalized_columns_initializer(1.0))
+      self.eigen_q_val = tf.matmul(tf.concat([self.value_features, self.current_option_direction], 1), value_embedding,
+                               name="fc_option_value")
+      self.eigen_q_val = tf.squeeze(self.eigen_q_val, 1)
+      self.summaries_option.append(tf.contrib.layers.summarize_activation(self.eigen_q_val))
+
+  """Build the intra-option policies"""
+  def build_intraoption_policies_nets(self):
+    with tf.variable_scope("option_pi"):
+      policy = tf.squeeze(tf.matmul(self.policy_features, tf.expand_dims(self.current_option_direction, 2)), 2)
+      policy = tf.contrib.layers.flatten(policy)
+      self.option_policy = tf.nn.softmax(policy, name="policy")
+
+      self.summaries_option.append(tf.contrib.layers.summarize_activation(self.option_policy))
+
+  """Build the option termination stochastic conditions"""
+  def build_option_term_net(self):
+    pass
+
+  """Build the option action-value functions"""
+  def build_option_q_val_net(self):
+    with tf.variable_scope("option_policy"):
+      out = tf.stop_gradient(self.fi_relu)
+      """If we accept primitive actions as options, we add action-value functions to those as well and we increase the number of units to include them at the end"""
+      direction_features = layers.fully_connected(out, num_outputs=self.goal_embedding_size,
+                                          activation_fn=None,
+                                          variables_collections=tf.get_collection("variables"),
+                                          outputs_collections="activations", scope="q_val")
+      self.summaries_critic.append(tf.contrib.layers.summarize_activation(direction_features))
+
+      content_match = tf.tensordot(direction_features, self.eigenvectors[0], axes=[[1], [1]])
+      self.attention_weights = tf.nn.softmax(content_match)
+
+      current_direction = tf.tensordot(self.attention_weights, self.eigenvectors[0], axes=[[1], [0]])
+      self.current_option_direction = tf.nn.l2_normalize(current_direction, axis=1)
+
+      self.summaries_critic.append(tf.contrib.layers.summarize_activation(self.current_option_direction))
+
+
+  def build_losses(self):
+    """Get the probabilities for each action taken under the intra-option policy"""
+    self.responsible_actions = self.get_responsible_actions(self.option_policy, self.actions_placeholder)
+
+    """Adding comparison of predicted frame and actual next frame to tensorboard"""
+    self.image_summaries.append(
+      tf.summary.image('next', tf.concat([self.next_obs, self.target_next_obs], 2), max_outputs=30))
+
+    """Building losses"""
+    with tf.name_scope('sf_loss'):
+      """TD error of successor representations"""
+      sf_td_error = self.target_sf - self.sf
+      self.sf_loss = tf.reduce_mean(self.config.sf_coef * huber_loss(sf_td_error))
+
+    with tf.name_scope('aux_loss'):
+      """L2 loss for the next frame prediction"""
+      aux_error = self.next_obs - self.target_next_obs
+      self.aux_loss = tf.reduce_mean(self.config.aux_coef * huber_loss(aux_error))
+
+    """If we use eigendirections for the options, than do TD on the eigen intra-option critics"""
+    with tf.name_scope('eigen_critic_loss'):
+      """Zero out where the option was a primitve one"""
+      eigen_td_error = self.target_eigen_return - self.eigen_q_val
+      self.eigen_critic_loss = tf.reduce_mean(0.5 * self.config.eigen_critic_coef * tf.square(eigen_td_error))
+
+    """Add an entropy regularization for each intra-option policy, driving exploration in the action space of intra-option policies"""
+    with tf.name_scope('entropy_loss'):
+      """Zero out primitive options"""
+      self.entropy_loss = -self.entropy_coef * tf.reduce_mean(self.option_policy * tf.log(self.option_policy + 1e-7))
+    """Learn intra-option policies with policy gradients"""
+    with tf.name_scope('policy_loss'):
+      self.policy_loss = -tf.reduce_mean(tf.log(self.responsible_actions + 1e-7) * tf.stop_gradient(
+                   eigen_td_error))
+
+    self.option_loss = self.policy_loss - self.entropy_loss + self.eigen_critic_loss
+
+  """Build gradients for the losses with respect to the network params.
+      Build summaries and update ops"""
+  def gradients_and_summaries(self):
+    local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+
+    """Gradients and update ops"""
+    self.grads_sf, self.apply_grads_sf = self.take_gradient(self.sf_loss)
+    self.grads_aux, self.apply_grads_aux = self.take_gradient(self.aux_loss)
+    self.grads_option, self.apply_grads_option = self.take_gradient(self.option_loss)
+
+    """Summaries"""
+    self.merged_summary_sf = tf.summary.merge(
+      self.summaries_sf + [tf.summary.scalar('avg_sf_loss', self.sf_loss),
+        gradient_summaries(zip(self.grads_sf, local_vars))])
+    self.merged_summary_aux = tf.summary.merge(self.image_summaries +
+                                               self.summaries_aux +
+                                               [tf.summary.scalar('aux_loss', self.aux_loss),
+                                                 gradient_summaries(zip(self.grads_aux, local_vars))])
+    options_to_merge = self.summaries_option +\
+                       [tf.summary.scalar('avg_entropy_loss', self.entropy_loss),
+                        tf.summary.scalar('avg_policy_loss', self.policy_loss),
+                        tf.summary.scalar('avg_eigen_critic_loss', self.eigen_critic_loss),
+                        gradient_summaries(zip(self.grads_option, local_vars),)]
+
+    self.merged_summary_option = tf.summary.merge(options_to_merge)
+
+
+  def build_network(self):
+    # if self.scope != 'global':
+    """Perform singular value decomposition on the SR matrix buffer.
+            Transopose eigenvectors and cojugate to be equivalent to the numpy decomposition"""
+    self.matrix_sf = tf.placeholder(shape=[1, self.config.sf_matrix_size, self.goal_embedding_size],
+                                    dtype=tf.float32, name="matrix_sf")
+    self.eigenvalues, _, ev = tf.svd(self.matrix_sf, full_matrices=False, compute_uv=True)
+    self.eigenvectors = tf.transpose(tf.conj(ev), perm=[0, 2, 1])
+
+    super(AttentionNetwork, self).build_network()
